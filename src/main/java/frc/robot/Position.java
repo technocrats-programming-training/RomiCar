3960297df2d2648520f33817c6c8b5ea3d70465f
Henry Scheible <henry.scheible@gmail.com>
2022-06-29 22:34:56 -0500

    Update changes

--- src/main/java/frc/robot/Position.java
+++ src/main/java/frc/robot/Position.java
@@ -1,22 +1,39 @@
 package frc.robot;
 
+import static lib.Romi.driveDistance;
+import static lib.Romi.turnDegrees;
+
 public class Position {
 
+    double xPosition = 0;
+    double yPosition = 0;
+    double positionalPointing = 0;
 
-    public void driveUp() {
-
+    public void driveUp() throws InterruptedException {
+        turnDegrees(-positionalPointing);
+        driveDistance(10);
+        yPosition += 10;
     }
 
-    public void driveDown() {
-
+    public void driveDown() throws InterruptedException {
+        turnDegrees(-positionalPointing);
+        driveDistance(-10);
+        yPosition -= 10;
     }
 
-    public void driveRight() {
-
+    public void driveRight() throws InterruptedException {
+        turnDegrees(90 - positionalPointing);
+        positionalPointing = 90;
+        driveDistance(10);
+        xPosition += 10;
     }
 
-    public void driveLeft() {
-
+    public void driveLeft() throws InterruptedException {
+        turnDegrees(-90 - positionalPointing);
+        driveDistance(10);
+        turnDegrees(-90);
+        positionalPointing = -90;
+        xPosition -= 10;
     }
 
     public void returnToHome() {
