48b33932f0fc4ab93f99fbafc20e33df2efadd4f
Henry Scheible <henry.scheible@gmail.com>
2022-06-30 13:25:56 -0500

    Remove solution code to prepare for template

--- src/main/java/frc/robot/Position.java
+++ src/main/java/frc/robot/Position.java
@@ -5,35 +5,21 @@ import static lib.Romi.turnDegrees;
 
 public class Position {
 
-    int xPosition = 0;
-    int yPosition = 0;
-    int positionalPointing = 0;
 
     public void driveUp() throws InterruptedException {
-        turnDegrees(-positionalPointing);
-        driveDistance(10);
-        yPosition += 10;
+
     }
 
     public void driveDown() throws InterruptedException {
-        turnDegrees(-positionalPointing);
-        driveDistance(-10);
-        yPosition -= 10;
+
     }
 
     public void driveRight() throws InterruptedException {
-        turnDegrees(90 - positionalPointing);
-        positionalPointing = 90;
-        driveDistance(10);
-        xPosition += 10;
+
     }
 
     public void driveLeft() throws InterruptedException {
-        turnDegrees(-90 - positionalPointing);
-        driveDistance(10);
-        turnDegrees(-90);
-        positionalPointing = -90;
-        xPosition -= 10;
+
     }
 
     public void returnToHome() {
@@ -41,6 +27,6 @@ public class Position {
     }
 
     public String toString() {
-        return "(" + xPosition + ", " + yPosition + ")";
+        return "";
     }
 }
